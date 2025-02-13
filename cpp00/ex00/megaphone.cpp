9cf56f5381312fcde19bc8bf29e43252cd2827fb
carolina-tcn <carolinatacconis@gmail.com>
2025-02-13 20:36:11 +0100

    ex00 con salto de linea

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -5,14 +5,38 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ctacconi <ctacconi@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/12 17:54:45 by ctacconi          #+#    #+#             */
-/*   Updated: 2025/02/12 18:06:56 by ctacconi         ###   ########.fr       */
+/*   Created: 2025/02/13 18:12:48 by ctacconi          #+#    #+#             */
+/*   Updated: 2025/02/13 19:42:06 by ctacconi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include <iostream> //to use cout
+#include <cctype>
+#include <string>
+//std es un namespace, que tiene definidos objetos y f(x) como cout, cin, endl, string, vector,,,
+//int main()
+//{ //insertion operator << to output/print text
+//	std::cout << "Hello Carola!" << std::endl; //sin \n
+//	return (0);
+//}
 
-int main(void)
+//si no hay args
+//* LOUD AND UNBEARABLE FEEDBACK NOISE *
+//si meto frase en min, pasa a MAY
+
+
+int main(int argc, char **argv)
 {
-	cout << "Carolina";
+	if (argc > 1)
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; argv; j++)
+				argv[i][j] = std::toupper(argv[i][j]);
+			std::cout << argv[i];
+		}
+	}
+	else
+		std::cout << "* LOUD AND UBEARABLE FEEDBACK NOISE *" << std::endl;
+	return(0);
 }
