3818c6f7786b90f2488d9fafe7a770478e040ef3
inakiurzz <inaki.u26@gmail.com>
2025-02-16 13:09:58 +0100

    include cctype

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #include <iostream>
-//#include <cctype>
+#include <cctype>
 
 int main(int argc, char **argv)
 {
