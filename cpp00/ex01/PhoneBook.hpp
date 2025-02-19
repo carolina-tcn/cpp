7d27fc379a8dd78bd61151fa29a16adc45186fa6
inakiurzz <inaki.u26@gmail.com>
2025-02-19 20:27:14 +0100

    .hpp phonebook

--- cpp00/ex01/PhoneBook.hpp
+++ cpp00/ex01/PhoneBook.hpp
@@ -3,17 +3,22 @@
 
 #include "Contact.hpp"
 
+#define MAX_CONTACTS 8
+
 class PhoneBook
 {	
 	private:
-		Contact _contacts[8];
-		int index;
+		Contact contacts[MAX_CONTACTS];
+		int contact_idx;
+		int contact_count;
 
-	public:/
+	public:
 		PhoneBook();
+		~PhoneBook();
+
 		void addContact();
 		void searchContact() const;
-		~PhoneBook();
+		
 };
 
 #endif
