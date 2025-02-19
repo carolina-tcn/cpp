4d177f161a4b0c5264c2cbedf24d15750dcd098c
inakiurzz <inaki.u26@gmail.com>
2025-02-19 20:26:43 +0100

    setcontact y displaycontact done

--- cpp00/ex01/Contact.cpp
+++ cpp00/ex01/Contact.cpp
@@ -1,6 +1,26 @@
 #include "Contact.hpp"
 
-Contact::Contact()
+Contact::Contact() : first_name(""), last_name(""), nickname(""), phone_number(""), darkest_secret("") {}
+
+void Contact::setContact()
 {
-	//Se accede al valor a través del getter, porque es private
+	std::cout << "Enter First Name: ";
+	std::getline(std::cin, first_name);
+	std::cout << "Enter Last Name: ";
+	std::getline(std::cin, last_name);
+	std::cout << "Enter Nickame: ";
+	std::getline(std::cin, nickname);
+	std::cout << "Enter Phone number: ";
+	std::getline(std::cin, phone_number);
+	std::cout << "Enter Darkest secret: ";
+	std::getline(std::cin, darkest_secret);
+}
+
+void Contact::displayContact() const
+{
+	std::cout << "First Name: " << first_name << std::endl;
+	std::cout << "Last Name: " << last_name << std::endl;
+	std::cout << "Nickname: " << nickname << std::endl;
+	std::cout << "Phone number: " << phone_number << std::endl;
+	std::cout << "Darkest secret: " << darkest_secret << std::endl;
 }
