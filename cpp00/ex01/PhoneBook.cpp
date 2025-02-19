92f2cbe01b1d6069fd97d1093930ab5983732ccc
inakiurzz <inaki.u26@gmail.com>
2025-02-19 20:28:28 +0100

    .cpp with % 8 to restart idx

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -1,6 +1,13 @@
 #include "PhoneBook.hpp"
 
-PhoneBook::PhoneBook() : index(0) {}
+PhoneBook::PhoneBook() : contact_idx(0), contact_count(0) {}
+
+void PhoneBook::addContact()
 {
-	
+	contacts[contact_idx].setContact();
+
+	if (contact_count < MAX_CONTACTS)
+		contact_count++;
+	contact_idx = (contact_idx + 1) % 8;
+
 }
