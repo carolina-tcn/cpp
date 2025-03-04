95d32b81ed7f8bd52fc57b7f56eaff783c17031c
inakiurzz <inaki.u26@gmail.com>
2025-03-04 19:08:46 +0100

    the memory addresses

--- cpp_01/ex02/main.cpp
+++ cpp_01/ex02/main.cpp
@@ -7,5 +7,8 @@ int main()
 	std::string* stringPTR = &var;
 	std::string& stringREF = var;
 
+	std::cout << "The memory address of the string variable: " << &var << std::endl
+		<< "The memory address held by stringPTR: " << stringPTR << std::endl
+		<< "The memory address held by stringREF: " << &stringREF << std::endl;
 	
 }
