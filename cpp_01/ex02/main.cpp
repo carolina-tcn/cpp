360e4e6693f9e5821f46dcaf38b78b6fdd2399b0
inakiurzz <inaki.u26@gmail.com>
2025-03-04 19:12:08 +0100

    ex02 done

--- cpp_01/ex02/main.cpp
+++ cpp_01/ex02/main.cpp
@@ -9,6 +9,9 @@ int main()
 
 	std::cout << "The memory address of the string variable: " << &var << std::endl
 		<< "The memory address held by stringPTR: " << stringPTR << std::endl
-		<< "The memory address held by stringREF: " << &stringREF << std::endl;
+		<< "The memory address held by stringREF: " << &stringREF << "\n" << std::endl;
 	
+	std::cout << "The value of the string variable: " << var << std::endl
+		<< "The value pointed to by stringPTR: " << *stringPTR << std::endl
+		<< "The value pointed to by stringREF: " << stringREF << std::endl;
 }
