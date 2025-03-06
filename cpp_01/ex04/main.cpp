23f41f97c7baedc34e839f384272be32f74e1e6e
inakiurzz <inaki.u26@gmail.com>
2025-03-06 21:25:23 +0100

    args done, started with how to read file

--- cpp_01/ex04/main.cpp
+++ cpp_01/ex04/main.cpp
@@ -1,32 +1,45 @@
 #include <string>
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
+
+/*REPLACE_STRINGS_FUNCTION
+{
+	std::string s1
+	std::string s2
+	s1 = argv[2];
+	s1 = argv[3];
+}*/
+
+
+std::string* readFile(std::string file)
+{
+	std::ifstream inFile(file); //open?
+	if (!) //no se abre
+		//exit
+
+	while (getline(MyReadFile, txt_file))
+	{
+		std::cout << txt_file;
+		std::cout << "\n";
+	}
+	std::cout << "el arg1 contiene: " << txt_file << std::endl;
+	MyReadFile.close();
+}
 
 
 //string:: length, substr, find, append, copy
 int main(int argc, char **argv)
 {
-	std::string txt_file;
-	std::string s1;
-	std::string s2;
-	
 	if (argc != 4)
-	{	std::cerr << "The program takes three parameters: filename, string1 and string2." << std::endl;
-		return (1);
-	}
+		return (std::cerr << "The program takes three parameters: <filename> <string1> <string2>." << std::endl);
+		
 	if (!(*argv[1] && *argv[2] && *argv[3]))
-	{
-		std::cerr << "The program needs three valid parameters." << std::endl; 
-		return (1);
-	}
-	s1 = argv[2];
-	s1 = argv[3];
+		return (std::cerr << "The program needs three valid parameters." << std::endl);
+
+	fileContent = readFile(*argv[1]);
+	if (!fileContent)
+		return (std::cerr << "The file is empty" << std::endl);
 	
-	std::ifstream MyReadFile(argv[1]);
-	while (getline(MyReadFile, txt_file))
-		std::cout << txt_file;
-	MyReadFile.close();
-
-
-	return (0);
+	return (EXIT_SUCCESS);
 }
