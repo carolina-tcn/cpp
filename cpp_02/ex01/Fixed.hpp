afe6920b370b847387d28945953d39d2f46d5805
inakiurzz <inaki.u26@gmail.com>
2025-03-17 21:05:50 +0100

    add comments

--- cpp_02/ex01/Fixed.hpp
+++ cpp_02/ex01/Fixed.hpp
@@ -1,32 +1,36 @@
 #ifndef FIXED_HPP
 # define FIXED_HPP
 
+#include <fstream>
 #include <iostream>
-
 
 class Fixed
 {
 	private:
+		//Members
 		int fixed_point_value;
 		static const int fractional_bits = 8;
 
 	public:
+		//Constructors
 		Fixed();
-		Fixed(const Fixed &fixed); //copy constructor
-
+		Fixed(const Fixed &fixed);
 		Fixed(const int value);
 		Fixed(const float value);
 
+		//Assignment operator
 		Fixed &operator = (const Fixed &fixed);//copy assignment operator overload
+
+		//Destructor
 		~Fixed();
 
+		//Member functions
 		int getRawBits() const;
 		void setRawBits(int const raw);
-
 		float toFloat() const;
 		int toInt() const;
 };
 
-
+std::ostream& operator<<(std::ostream &out, const Fixed& fixed);
 
 # endif
