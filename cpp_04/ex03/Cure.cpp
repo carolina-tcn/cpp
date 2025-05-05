d249527df5ab00cca93c4f6636fcca09a5bdebe3
carolina-tcn <carolinatacconis@gmail.com>
2025-05-05 11:51:19 +0200

    cure implementation

--- cpp_04/ex03/Cure.cpp
+++ cpp_04/ex03/Cure.cpp
@@ -6,13 +6,13 @@
 /*   By: carolinatacconis <carolinatacconis@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/04 14:15:17 by carolinatac       #+#    #+#             */
-/*   Updated: 2025/05/05 11:43:29 by carolinatac      ###   ########.fr       */
+/*   Updated: 2025/05/05 11:50:42 by carolinatac      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Cure.hpp"
 
-Cure::Cure() : _type("ice")
+Cure::Cure() : AMateria("cure")
 {
 	std::cout << YELLOW << "Cure created." << RESET << std::endl;
 }
@@ -36,18 +36,12 @@ Cure::~Cure()
 	std::cout << YELLOW << "Cure destroyed." << RESET << std::endl;	
 }
 
-std::string	const &Cure::getType() const
-{
-	return(_type);
-}
-
 AMateria* Cure::clone() const
 {
-	const AMateria* materia = new Cure;
-	Cure(*this);
+	return (new Cure(*this));
 }
 
 void Cure::use(ICharacter& target)
 {
-	std::cout << YELLOW << "* shoots an ice bolt at " << target << RESET << std::endl;
+	std::cout << YELLOW << "* heals " << target.getName() << "'s wounds *"<< RESET << std::endl;
 }
--- cpp_04/ex03/Cure.hpp
+++ cpp_04/ex03/Cure.hpp
@@ -6,7 +6,7 @@
 /*   By: carolinatacconis <carolinatacconis@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/04 14:15:20 by carolinatac       #+#    #+#             */
-/*   Updated: 2025/05/05 11:37:55 by carolinatac      ###   ########.fr       */
+/*   Updated: 2025/05/05 11:46:10 by carolinatac      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,11 @@
 
 class Cure : public AMateria
 {
-	protected:
-		std::string	_type;
-
 	public:
 		Cure();
 		Cure(const Cure &other);
 		Cure&	operator=(const Cure &other);
 		~Cure();
-
-		std::string	const &getType() const;
 
 		virtual AMateria* clone() const;
 		virtual void use(ICharacter& target);
