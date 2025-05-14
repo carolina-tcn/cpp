a4f57040aace23912275b97f28e293fbec0be4c9
carolina-tcn <carolinatacconis@gmail.com>
2025-05-14 19:46:18 +0200

    ascii tree created

--- cpp_05/ex02/AForm/ShrubberyCreationForm/ShrubberyCreationForm.cpp
+++ cpp_05/ex02/AForm/ShrubberyCreationForm/ShrubberyCreationForm.cpp
@@ -6,33 +6,52 @@
 /*   By: ctacconi <ctacconi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 20:57:50 by ctacconi          #+#    #+#             */
-/*   Updated: 2025/05/14 18:22:34 by ctacconi         ###   ########.fr       */
+/*   Updated: 2025/05/14 19:45:55 by ctacconi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ShrubberyCreationForm.hpp"
+#include <fstream>
 
-ShrubberyCreationForm::ShrubberyCreationForm()
-{
-    
-}
+ShrubberyCreationForm::ShrubberyCreationForm() : _target("") {}
 
-ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm &other)
-{
-    
-}
+ShrubberyCreationForm::ShrubberyCreationForm(const std::string& target) : 
+    AForm("Shrubbery Creation Form", 145, 137), _target(target) {}
+
+ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm &other) :
+    AForm("Shrubbery Creation Form", 145, 137), _target(other._target) {}
 
 ShrubberyCreationForm& ShrubberyCreationForm::operator=(const ShrubberyCreationForm &other)
 {
-    
+    if (this != &other)
+    {
+        _target = other._target;
+    }
+    return (*this);
 }
 
-ShrubberyCreationForm::~ShrubberyCreationForm()
+ShrubberyCreationForm::~ShrubberyCreationForm() {}
+
+void ShrubberyCreationForm::execute(Bureaucrat const &executor) const
 {
-    
+    if(this->getSigned() == false)
+        throw FormNotSignedException();
+    if (executor.getGrade() > this->getGradeExec())
+        throw GradeTooHighException();
+    std::ofstream target_file(_target + "_shrubbery");
+    if (!target_file.is_open())
+    {
+        std::cerr << "Error: Could not create the file." << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    target_file << "     ###     " << std::endl;
+    target_file << "    #o###    " << std::endl;
+    target_file << "  #####o###  " << std::endl;
+    target_file << " #o#\#|#/### " << std::endl;
+    target_file << "  ###\|/#o#  " << std::endl;
+    target_file << "   # }|{ #   " << std::endl;
+    target_file << "     }|{     " << std::endl;
+    target_file << "     }|{     " << std::endl;
+    target_file.close();
 }
 
-void ShrubberyCreationForm::execute(Bureaucrat const &executor)
-{
-    
-}
