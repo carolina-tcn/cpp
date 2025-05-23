01ad4e147652d037a5b8a848fb940f4441ba7bf7
carolina-tcn <carolinatacconis@gmail.com>
2025-05-23 19:59:27 +0200

    toChar done

--- cpp_06/ex00/ScalarConverter.cpp
+++ cpp_06/ex00/ScalarConverter.cpp
@@ -6,7 +6,7 @@
 /*   By: ctacconi <ctacconi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 16:53:32 by ctacconi          #+#    #+#             */
-/*   Updated: 2025/05/23 19:49:43 by ctacconi         ###   ########.fr       */
+/*   Updated: 2025/05/23 19:56:04 by ctacconi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,19 +27,12 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter &other)
 
 ScalarConverter::~ScalarConverter() {}
 
-// cadena no es convertible, mientras que stoi genera una excepción.
-//atoi: Devuelve 0 en caso de error (cadena no convertible).
-
-//int atoi (const char *str);
-
-//1.detect type of the literal passed
-    
+//1.detect type of the literal passed 
 //2.convert from string to actual type
     double d = 42.0;
     float f = static_cast<float>(d);
     int i = static_cast<int>(d);
     char c = static_cast<char>(d);
-
 //3.convert explicitly to the 3 other types
 //4.display the results
 
@@ -49,10 +42,7 @@ bool    ScalarConverter::handlePseudoLiteral(const std::string& literal)
     if (literal == "nan" || literal == "nanf" || literal == "inf" ||
         literal == "+inf" || literal == "-inf" || literal == "inff" ||
         literal == "+inff" || literal == "-inff" )
-    {
-        std::cout << "impossible bla bla bla" << std::endl;
         return (true);
-    }
     return (false);
 }
 
@@ -63,10 +53,6 @@ bool    ScalarConverter::isChar(const std::string& literal)
     return (false);
 }
 
-//long int de 64 bits
-//long int strtol (const char* str, char** endptr, int base);
-//double strtod (const char* str, char** endptr);
-//ERANGE Result too large (POSIX.1, C99).
 bool    ScalarConverter::isNumber(const std::string& literal)
 {
     int dotCount = 0;
@@ -103,22 +89,8 @@ bool    ScalarConverter::isNumber(const std::string& literal)
                 return (false);
         }
     }
-    return (hasDigit);
-    // value = std::strtod(toCheck.c_str(), &end); // devuelve un const char* estilo C a partir de std::string
-    // if (*end != '\0')
-    //     return (std::cout << "number: " << ERR_INVALID_INPUT << std::endl, false);
-    // if (errno == ERANGE)
-    //     return (std::cout << ERR_OVERFLOW_UNDERFLOW << std::endl, false);
-
-    // std::cout << "Number conversion succeed: " << value << std::endl;
-    // return (true);   
+    return (hasDigit); 
 }
-
-// bool    ScalarConverter::isFloat(const double &value)
-// {
-//     std::cout << "VALUE: " << value << std::endl;
-//     return (true);
-// }
 
 void ScalarConverter::convertToChar(const std::string& literal, const ScalarType &inputType)
 {
@@ -157,11 +129,6 @@ void ScalarConverter::convertToChar(const std::string& literal, const ScalarType
 
 ScalarType  ScalarConverter::detectType(const std::string& literal)
 {
-    std::cout << "The literal is: " << literal << std::endl;
-
-    size_t size_literal = literal.size();
-    std::cout << "Length of the string: " << size_literal << std::endl;
-
     if (handlePseudoLiteral(literal))
         return (TYPE_PSEUDO);
     if (isChar(literal))
@@ -193,9 +160,9 @@ void    ScalarConverter::convert(const std::string& literal)
         return ;
     }
     convertToChar(literal, inputType);
-    // convertToInt(literal, inputType);
-    // convertToFloat(literal, inputType);
-    // convertToDouble(literal, inputType);
+    //convertToInt(literal, inputType);
+    //convertToFloat(literal, inputType);
+    //convertToDouble(literal, inputType);
     std::cout << "OK" << std::endl;
 }
 
