5728b77d182aebb65ba1309ded340fab436d50a3
carolina-tcn <carolinatacconis@gmail.com>
2025-05-28 17:38:36 +0200

    ex01

--- cpp_07/ex01/iter.hpp
+++ cpp_07/ex01/iter.hpp
@@ -6,14 +6,17 @@
 /*   By: ctacconi <ctacconi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 20:47:28 by ctacconi          #+#    #+#             */
-/*   Updated: 2025/05/27 20:48:24 by ctacconi         ###   ########.fr       */
+/*   Updated: 2025/05/28 17:29:23 by ctacconi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-template <typename T>
-void    iter(T address, T length, T f)
+#include <cstddef>
+
+template <typename T, typename F>
+void    iter(T* array, size_t length, F function)
 {
-    
+    for(size_t i = 0; i < length; ++i)
+        function(array[i]);
 }
--- cpp_07/ex01/main.cpp
+++ cpp_07/ex01/main.cpp
@@ -6,13 +6,42 @@
 /*   By: ctacconi <ctacconi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/27 20:46:59 by ctacconi          #+#    #+#             */
-/*   Updated: 2025/05/27 20:47:22 by ctacconi         ###   ########.fr       */
+/*   Updated: 2025/05/28 17:37:13 by ctacconi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
+#include <iostream>
+#include <string>
+
+template <typename T>
+void    printElement(const T& element)
+{
+    std::cout << element << " ";
+}
 
 int main()
 {
+    std::string str[] = {"hola", "carol", "que", "tal"};
+    int arr[] = {1, 2, 3, 4, 5};
+    char c[] = {'a', 'b', 'c', 'd', 'e'};
+    float f[] = {1.1f, 2.3f, 5.0f};
+
+    std::cout << "str: ";
+    iter(str, 4, printElement<std::string>);
+    std::cout << std::endl;
+
+    std::cout << "arr: ";
+    iter(arr, 5, printElement<int>);
+    std::cout << std::endl;
+
+    std::cout << "c: ";
+    iter(c, 5, printElement<char>);
+    std::cout << std::endl;
+
+    std::cout << "f: ";
+    iter(f, 3, printElement<float>);
+    std::cout << std::endl;
+
     return (0);   
 }
